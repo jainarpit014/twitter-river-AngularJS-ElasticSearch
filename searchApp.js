a0842477ba3dcffd269b21e9ccf76d70a3a5cbde
jainarpit014 <jainarpit014@yahoo.in>
2014-06-16 17:28:21 +0530

    Create searchApp.js
    This files defines the required modules and route options.Twitter.html is view for TwitterController

---
+++ searchApp.js
@@ -0,0 +1,18 @@
+/**
+* angular.module defines a module for our applicatoin, 'searchApp', with
+* an array of the modules searchApp depends on.
+* elasticjs.service is a javascript client for elasticsearch
+* ngSanitize angularjs module to sanitize HTML
+*/
+var searchApp = angular.module('searchApp', [
+    'elasticjs.service',
+    'ngSanitize',
+    'infinite-scroll'
+])
+    .config(['$routeProvider', function($routeProvider){
+        $routeProvider
+            .when('/', {
+                controller: 'Twitter',
+                templateUrl: 'views/twitter.html'
+            })
+    }]);
